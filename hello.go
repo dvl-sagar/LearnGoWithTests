a340f1d9a8d5b564708c749800a4c848780c8d74
dvl-sagar <sagar.sharma@digivatelabs.com>
2024-11-28 12:35:30 +0530

    Spanish logic added

--- hello.go
+++ hello.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string{
-	return "Hello, " + name
+func Hello(name string, language string) string{
+	if name == "" {
+		name = "World"
+	}
+	if language == spanish{
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
 }
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "english"))
 }
