febd89318d77f200b0cdadfb1bdb76100c7e219b
dvl-sagar <sagar.sharma@digivatelabs.com>
2024-11-28 12:42:14 +0530

    Hello, World Completed

--- hello.go
+++ hello.go
@@ -2,16 +2,23 @@ package main
 
 import "fmt"
 
+
+const french = "French"
 const spanish = "Spanish"
+
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string{
 	if name == "" {
 		name = "World"
 	}
-	if language == spanish{
-		return spanishHelloPrefix + name
+	switch language{
+	case spanish:
+		return spanishHelloPrefix+name
+	case french:
+		return frenchHelloPrefix+name
 	}
 	return englishHelloPrefix + name
 }
