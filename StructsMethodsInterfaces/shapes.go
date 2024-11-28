88aa443f3943ea96d34ab08ca9859df4482c22ed
dvl-sagar <sagar.sharma@digivatelabs.com>
2024-11-28 18:01:32 +0530

    StructMethodInterface dir added

---
+++ StructsMethodsInterfaces/shapes.go
@@ -0,0 +1,9 @@
+package StructMethodsInterface
+
+func Perimeter(breadth float64, length float64) float64{
+	return 2*(breadth + length)
+}
+
+func Area(breadth, length float64)float64{
+	return breadth * length
+}
