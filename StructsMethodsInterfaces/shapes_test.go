757facf73cf882175adadf7b64da60a41127ebc5
dvl-sagar <sagar.sharma@digivatelabs.com>
2024-11-29 09:40:58 +0530

    t.Run used for test cases

--- StructsMethodsInterfaces/shapes_test.go
+++ StructsMethodsInterfaces/shapes_test.go
@@ -13,18 +13,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	testCases := []struct{
+	testCases := []struct {
+		name  string
 		shape Shape
-		want float64
+		want  float64
 	}{
-		{Rectangle{10,10},100},
-		{Circle{10},314.1592653589793},
-		{Triangle{10, 6}, 30.0},
+		{"Rectangle", Rectangle{10, 10}, 100},
+		{"Circle", Circle{10}, 314.1592653589793},
+		{"Triangle", Triangle{10, 6}, 30.0},
 	}
-	for _,tt := range testCases{
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("got %g want %g", got, tt.want)
+			}
+		})
 	}
 }
