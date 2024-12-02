7460b5ef9699d18b1116785260ea555d95091280
sagar sharma <sagar.sharma@digivatelabs.com>
2024-12-03 01:31:54 +0530

    inported external library for templates

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module LearnGoWithTests
 
 go 1.23.2
 
-require github.com/quii/learn-go-with-tests v0.0.0-20241122141118-44f983b2eed5
+require (
+	github.com/approvals/go-approval-tests v0.0.0-20211008131110-0c40b30e0000
+	github.com/gomarkdown/markdown v0.0.0-20240626202925-2eda941fd024
+	github.com/quii/learn-go-with-tests v0.0.0-20241122141118-44f983b2eed5
+)
---
+++ learnTemplates/renderer.go
@@ -0,0 +1,66 @@
+package blogrenderer
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
+)
+
+var (
+	postTemplates embed.FS
+)
+
+// PostRenderer renders data into HTML
+type PostRenderer struct {
+	templ    *template.Template
+	mdParser *parser.Parser
+}
+
+// Post is a representation of a post
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+// SanitisedTitle returns the title of the post with spaces replaced by dashes for pleasant URLs
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.Replace(p.Title, " ", "-", -1))
+}
+
+// NewPostRenderer creates a new PostRenderer
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	parser := parser.NewWithExtensions(extensions)
+
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
+}
+
+// Render renders post into HTML
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r))
+}
+
+// RenderIndex creates an HTML index page given a collection of posts
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
+
+type postViewModel struct {
+	Post
+	HTMLBody template.HTML
+}
+
+func newPostVM(p Post, r *PostRenderer) postViewModel {
+	vm := postViewModel{Post: p}
+	vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return vm
+}
