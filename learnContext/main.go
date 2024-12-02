8c2b0f82f481351f9548939730a84afcd1fc27a7
sagar sharma <sagar.sharma@digivatelabs.com>
2024-12-02 18:32:25 +0530

    context initiated

---
+++ learnContext/main.go
@@ -0,0 +1,40 @@
+package learncontext
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
+
+type Store interface {
+	Fetch() string
+}
+
+type SpyStore struct {
+	response string
+}
+
+func (s *SpyStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+	svr := Server(&SpyStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
+	}
+}
